b7e11b02fa08bb77f24f7b4a017143b9ca37b545
AJ Keller <aj@pushtheworld.us>
2015-12-16 14:40:49 -0500

    Changes: Refactored index.js page Refactored OpenBCIBoard.js

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
-var serialport = require('serialport');
-var openBCIBoard = require("./OpenBCIBoard");
+var OpenBCIBoard = require("./OpenBCIBoard").OpenBCIBoard;
 
-var board = {
-  baudrate : 115200,
-  port: "/dev/ttyUSB0"
-};
+var serialPortName = "/dev/ttyUSB0";
 
-var ourSerial = new openBCIBoard.createBoard(board);
+var openBCIBoard = new OpenBCIBoard(serialPortName);
 
-ourSerial.open(function(err){
-  if (err){
-    console.log(err);
-  } else {
-    console.log("open");
-    ourSerial.on("data", function(data){
-      console.log(data.toString());
-    });
-    ourSerial.write('b');
-  }
-});
+
 
 process.on('SIGINT', function() {
     console.log("Caught interrupt signal");
-    if (ourSerial){
-        ourSerial.close();
+    if (openBCIBoard){
+        openBCIBoard.close();
     }
     process.exit();
 });
--- openBCIBoard.js
+++ openBCIBoard.js
@@ -1,24 +1,121 @@
-var serialport = require('serialport');
-module.exports = {
-    streaming: false,
-    serial: null,
-    createBoard: function(board){
-        return new serialport.SerialPort(board.port, {
-            baudrate: board.baudrate,
-            parser: serialport.parsers.byteLength(33)
-        });
-    },
-    listPorts: function() {
-        serialport.list(function (err, ports) {
-            if (err) {
-                return err;
-            } else {
-                return ports;
-            }
-        });
+var openBCISample = require('OpenBCISample');
+var k = require('OpenBCIConstants');
+var serialPort = require('serialport');
+var Serialport = serialPort.SerialPort;
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+
+function OpenBCIFactory() {
+    var factory = this;
+
+    var _options = {
+        baudrate: 115200,
+        daisy: false
+    };
+
+    function OpenBCIBoard(portName,options,callback) {
+        var self = this;
+
+        var args = Array.prototype.slice.call(arguments);
+
+        // Get callback
+        callback = args.pop();
+
+        if (typeof (callback) !== 'function') {
+            callback = null;
+        }
+        options = (typeof options !== 'function') && options || {};
+
+        var opts = {};
+
+        callback = callback || function(err) {
+                if(err) {
+                    if(self._events.error) {
+                        self.emit('error', err);
+                    } else {
+                        factory.emit('error', err);
+                    }
+                }
+            };
+
+        var err;
+
+        opts.baudRate = options.baudRate || options.baudrate || _options.baudrate;
+
+        opts.daisy = options.daisy || options.daisy || _options.daisy;
+
+        if(!portName) {
+            err = new Error('Invalid port specified: ' + portName);
+            callback(err);
+            return;
+        }
+        this.options = opts;
+        this.portName = portName;
     }
+
+    //OpenBCIBoard.prototype.listPorts = serialPort.listPorts;
+
+    OpenBCIBoard.prototype.boardConnect = function() {
+        var self = this;
+        this.paused = false;
+        this.connected = true;
+        var boardSerial = new Serialport(this.portName, {
+            baudrate: this.baudrate,
+            parser: serialport.parsers.byteLength(33);
+        });
+
+        boardSerial.on('open', function() {
+            console.log('open');
+            self.connected = true;
+            boardSerial.on('data', function(data) {
+                console.log('data recieved: ' + data);
+                self.emit('sample', OpenBCISample.convertPacketToSample(data);)
+            });
+        });
+        boardSerial.on('close', function() {
+            self.connected = false;
+            console.log('serial connection closed');
+        });
+        this.serial = boardSerial;
+
+        writeAndDrain(boardSerial, k.kOBCIStreamStart);
+
+    };
+
+    OpenBCIBoard.prototype.boardDisconnect = function() {
+        this.connected = false;
+        if(this.serial.isOpen()) {
+            this.serial.close();
+        }
+    };
+
+    /**
+     * Used to send a start signal to a connected device
+     * @param callback - contains an error message
+     */
+    OpenBCIBoard.prototype.streamStart = function(callback) {
+        if(this.connected) {
+            if(this.streaming === false) {
+                this.streaming = true;
+                writeAndDrain(this.serial,k.kOBCIStreamStart);
+            } else {
+                callback("All ready streaming")
+            }
+        } else {
+            callback("No device connected");
+        }
+        if (callback) { callback(); }
+    };
+
+    OpenBCIBoard.prototype.streamStop = function() {
+        if(this.streaming) {
+            this.streaming = false;
+            writeAndDrain(this.serial,k.kOBCIStreamStop);
+        }
+    };
     // TODO: boardCheckConnection (py: check_connection)
-    // TODO: boardDisconnect (py: disconnect)
+
     // TODO: boardReconnect (py: reconnect)
     // TODO: filtersDisable (py: disable_filters)
     // TODO: filtersEnable (py: enable_filters)
@@ -29,14 +126,16 @@ module.exports = {
     // TODO: printRegisterSettings (py: print)register_settings)
     // TODO: printBytesIn (py: print_bytes_in)
     // TODO: printPacketsIn (py: print_packets_in)
-    // TODO: streamStart (py: start_streaming)
-    // TODO: streamStop (py: stop)
     // TODO: warn
-    // TODO:
-};
 
-function writeAndDrain(data, callback) {
-    serialport.write(data, function() {
-        serialport.drain(callback);
+}
+
+util.inherits(OpenBCIFactory, EventEmitter);
+
+module.exports = new OpenBCIFactory();
+
+function writeAndDrain(boardSerial,data, callback) {
+    boardSerial.write(data, function() {
+        boardSerial.drain(callback);
     })
 }
