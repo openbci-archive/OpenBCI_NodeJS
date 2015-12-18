1ae8336dea08f6d6262d10c2a1587325430e07cb
AJ Keller <aj@pushtheworld.us>
2015-12-18 13:16:18 -0500

    Changes index.js -- major refractor still under way openBCIBoard.js -- check in for safety

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
 //var openBCIBoard = ;
 var board = require('./OpenBCIBoard');
 
-var serialPortName = "/dev/tty.usbserial-DN0095GD";
+var bciBoard = new board.OpenBCIBoard();
 
-var bciBoard = new board.OpenBCIBoard(serialPortName);
-
-bciBoard.boardConnect().then(function() {
-    setTimeout(function(){
-        console.log("Starting stream");
-        return bciBoard.streamStart().then(function(sample) {
-            console.log('Sample: ' + JSON.stringify(sample));
-        });
-    },1000);
-}, function(err) {
-    console.log('Error [Outside]: ' + err);
+bciBoard.autoFindOpenBCIBoard(function(portName) {
+    if(portName) {
+        bciBoard.boardConnect(portName).then(function() {
+                console.log('Waiting for 1 second before sending start stream command!');
+                setTimeout(function(){
+                    console.log("Starting stream");
+                    return bciBoard.streamStart().then(function(sample) {
+                        console.log('Sample: ' + JSON.stringify(sample));
+                    });
+                },1000);
+            })
+            .catch( function(err) {
+                console.log('Error [Outside]: ' + err);
+            });
+    } else {
+        /** Display list of ports*/
+    }
 });
+
+
+
+
+
+//bciBoard.autoFindOpenBCIBoard()
+//    .then(function(portName) {
+//        return new Promise(function (resolve, reject) {
+//            if (portName) {
+//                resolve(portName);
+//            } else {
+//                //reject('Unable to find device to connect to...');
+//            }
+//        });
+//    })
+//    .then(bciBoard.boardConnect).then(function() {
+//        console.log('Waiting for 1 second before sending start stream command!');
+//        setTimeout(function(){
+//            console.log("Starting stream");
+//            return bciBoard.streamStart().then(function(sample) {
+//                console.log('Sample: ' + JSON.stringify(sample));
+//            });
+//        },1000);
+//    })
+//    .catch( function(err) {
+//        console.log('Error [Outside]: ' + err);
+//});
 
 //setTimeout(function() {
 //    bciBoard.boardConnect().then(function(boardSerial) {
--- openBCIBoard.js
+++ openBCIBoard.js
@@ -19,17 +19,10 @@ function OpenBCIFactory() {
         parser: serialPort.parsers.byteLength(33)
     };
 
-    function OpenBCIBoard(portName,options,connectImmediately,callback) {
+    function OpenBCIBoard(portName,options,connectImmediately) {
         var self = this;
-
         var args = Array.prototype.slice.call(arguments);
 
-        // Get callback
-        callback = args.pop();
-
-        if (typeof (callback) !== 'function') {
-            callback = null;
-        }
         options = (typeof options !== 'function') && options || {};
 
         var opts = {};
@@ -37,6 +30,18 @@ function OpenBCIFactory() {
         connectImmediately = (connectImmediately === undefined || connectImmediately === null) ? true : connectImmediately;
 
         stream.Stream.call(this);
+
+        opts.baudRate = options.baudRate || options.baudrate || _options.baudrate;
+
+        opts.daisy = options.daisy || options.daisy || _options.daisy;
+
+        self.badPackets = 0;
+        self.options = opts;
+        self.portName = portName;
+
+    }
+
+
 
         //callback = callback || function(err) {
         //        if(err) {
@@ -47,78 +52,90 @@ function OpenBCIFactory() {
         //            }
         //        }
         //    };
+        //
+        //var err;
+        //
+        //
+        //
+        //if(portName === undefined || portName === null) {
+        //    this.portName = autoFindOpenBCIBoard();
+        //    console.log(this.portName);
+        //} else {
+        //    this.portName = portName;
+        //}
+        //if(!portName) {
+        //    err = new Error('Invalid port specified: ' + portName);
+        //    callback(err);
+        //    return;
+        //}
 
-        var err;
 
-        opts.baudRate = options.baudRate || options.baudrate || _options.baudrate;
+        //this.portName = portName;
 
-        opts.daisy = options.daisy || options.daisy || _options.daisy;
-
-        if(!portName) {
-            err = new Error('Invalid port specified: ' + portName);
-            callback(err);
-            return;
-        }
-        this.badPackets = 0;
-        this.options = opts;
-        this.portName = portName;
-        this.serialPort = serialPort;
         //this.parsers = serialPort.parsers;
         //if(connectImmediately) {
         //    process.nextTick(function() {
         //        self.boardConnect();
         //    });
         //}
-    }
+    //}
 
     util.inherits(OpenBCIBoard, stream.Stream);
     //OpenBCIBoard.prototype.listPorts = serialPort.listPorts;
 
-    OpenBCIBoard.prototype.boardConnect = function() {
+    OpenBCIBoard.prototype.boardConnect = function(portName) {
         var self = this;
         return new Promise(function(resolve, reject) {
-            self.paused = false;
+            //console.log('inside the promise, board is: ' + board);
+            //self.paused = false;
             self.connected = true;
             self.streaming = false;
             var moneyBuf = new Buffer('$$$');
-            console.log('Attempting to open serial connection to: ' + self.portName);
-            var boardSerial = new serialPort.SerialPort(self.portName, {
+            console.log('Attempting to open serial connection to: ' + portName + ' at baud rate: ' + self.options.baudRate);
+            var boardSerial = new serialPort.SerialPort(portName, {
                 baudrate: self.options.baudRate,
                 parser: serialPort.parsers.raw
             });
             self.serial = boardSerial;
-            boardSerial.on('open',function() {
+            boardSerial.on('open', function () {
                 console.log('Successful connection to board!');
+                console.log('Sending soft reset to board...');
+                boardSerial.write(k.OBCIMiscSoftReset, function(error, results) {
+                    if(results)
+                    console.log('    Success');
+                });
             });
 
-            var temp = function(data) {
-                var sizeOfData =  data.length;
-                for(var i = 0; i < sizeOfData - 2; i++) {
-                    if(moneyBuf.equals(data.slice(i,i+3))) {
+            var temp = function (data) {
+                var sizeOfData = data.length;
+                console.log(data);
+                for (var i = 0; i < sizeOfData - 2; i++) {
+                    if (moneyBuf.equals(data.slice(i, i + 3))) {
                         console.log('Money!');
                         return true;
                         //resolve(boardSerial);
                     }
                 }
-            }
+            };
 
-            boardSerial.on('data', function(data) {
-                //var sizeOfData =  data.length;
+            boardSerial.on('data', function (data) {
+                var sizeOfData =  data.length;
                 //console.log('Size of data: ' + sizeOfData.toString());
                 //console.log(data);
-
-                if(self.streaming === false) {
-                    if(temp(data)) {
+                //console.log('data recieved: ' + data);
+                if (self.streaming === false) {
+                    if (temp(data)) {
                         console.log('og data fired');
-                        boardSerial.removeListener('data',temp);
-                        boardSerial.flush(function() {
+                        boardSerial.removeListener('data', temp);
+                        boardSerial.flush(function () {
                             resolve(boardSerial);
                         });
                     }
                 }
 
             });
-            self.serial = boardSerial;
+
+            //board.serial = boardSerial;
         }).catch(function(err) {
             return Promise.reject('Error [boardConnect]' + err);
         });
@@ -142,15 +159,17 @@ function OpenBCIFactory() {
     OpenBCIBoard.prototype.streamStart = function() {
         var self = this;
         if(self.streaming === false) {
+            console.log('Streaming: true');
             self.streaming = true;
             console.log('telling board to start streaming');
+            self.serial.parser = serialPort.parsers.byteLength(33);
             writeAndDrain(self.serial, k.OBCIStreamStart);
         }
         return new Promise(function(resolve) {
             //self.serial.removeAllListners(['data']);
             //console.log(serialPort.parsers.byteLength(33) === self.serial.parser);
             self.serial.on('data', function(data) {
-                console.log('new era data recieved: ' + data);
+                //console.log('new era data recieved: ' + data);
                 resolve(data);
             });
             self.serial.on('close',function() {
@@ -215,6 +234,28 @@ function OpenBCIFactory() {
         }
     };
 
+    OpenBCIBoard.prototype.autoFindOpenBCIBoard = function(callback) {
+        var macSerialPrefix = 'usbserial-D';
+        var self = this;
+        serialPort.list(function(err, ports) {
+            console.log('Searching for ports...');
+            var foundPort = false;
+            ports.forEach(function (port) {
+                if (port.comName.search(macSerialPrefix) > 0) {
+                    self.portName = port.comName;
+                    console.log('found');
+                    callback(port.comName);
+                    foundPort = true;
+                }
+            });
+            if(!foundPort) {
+                callback();
+            }
+            //return false;
+            //reject('unable to auto locate bci device');
+        })
+    };
+
     // TODO: boardConnectAutomatic
     // TODO: boardCheckConnection (py: check_connection)
     // TODO: boardReconnect (py: reconnect)
@@ -244,13 +285,14 @@ module.exports = new OpenBCIFactory();
  */
 function writeAndDrain(boardSerial,data) {
     return new Promise(function(resolve,reject) {
+        //console.log(boardSerial);
         boardSerial.write(data,function(error,results) {
             if(results) {
-                //console.log('Sent msg to board');
+                console.log('Sent msg to board');
                 boardSerial.drain(function() {
                     //console.log('boardSerial in writeAndDrain: ');
                     //console.log(JSON.stringify(boardSerial));
-                    resolve(boardSerial);
+                    //resolve(boardSerial);
                 });
             } else {
                 console.log('Error [writeAndDrain]: ' + error);
@@ -262,5 +304,4 @@ function writeAndDrain(boardSerial,data) {
     //    boardSerial.drain(callback);
     //})
 }
-
 
